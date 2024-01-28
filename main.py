eeffbbfd410f041b0142b53da584a5a7e8830de8
Samuel Henao <codinsamueldev@gmail.com>
2024-01-28 17:00:23 -0500

    Instance of oauth2 and endpoint to secure
    Security not completed

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 app = FastAPI()
 app.title = "API Security"
+
+# This class receives a token as parameter, so what we are saying here is that we have an endpoint called "token" which will return the required information.
+oauth2_scheme = OAuth2PasswordBearer("/token")
 
 
 @app.get("/", tags=["Root"])
@@ -15,7 +20,11 @@ def welcome():
                     <li>When I was your man - Bruno Mars</li>
                     <li>Scars 2020 - Papa Roach</li>
                 </ul>
-            </article>
-            """)
+            </article>""")
+
+
+@app.get("/users/me", tags=["Users"])
+def user(token: str = Depends(oauth2_scheme)): # Depends is a function which will be executed when we use this endpoint.
+    return "Hey User!"
 
 
