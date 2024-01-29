c3caf5bf5bc2f308e351ee713871a0eee54db0a7
Samuel Henao <codinsamueldev@gmail.com>
2024-01-29 17:52:44 -0500

    new: separate db info to a new file

---
+++ fake_database.py
@@ -0,0 +1,10 @@
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    }
+}
+
