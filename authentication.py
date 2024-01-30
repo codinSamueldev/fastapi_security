ca5d48b559a53163115e2ee2d57d051d3df5eaeb
Samuel Henao <codinsamueldev@gmail.com>
2024-01-30 16:32:46 -0500

    feat: add jwt security flow

--- authentication.py
+++ authentication.py
@@ -1,7 +1,29 @@
+import os
+from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException
+from jose import JWTError, jwt
 from pydantic import BaseModel
 from typing import Union
+from dotenv import load_dotenv
+from passlib.context import CryptContext
 from fake_database import fake_users_db
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+
+class Token(BaseModel):
+    """ Define a Pydantic Model that will be used in the token endpoint for the response. """
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
 
 
 class User(BaseModel):
@@ -16,9 +38,21 @@ class UserInDB(User):
     hashed_password: str
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # Create a PassLib "context". This is what will be used to hash and verify passwords.
+
+
 
 def fake_hash_password(password: str):
     return "fakehashed" + password
+
+
+def verify_password(plain_password, hashed_password):
+    """ Verify if a received password matches the hash stored. Returns boolean value. """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 
@@ -33,17 +67,38 @@ def get_user(db, username):
     return []
 
 
-def authenticate_user(login_data):
-    user_dict = fake_users_db.get(login_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password.")
 
-    user = UserInDB(**user_dict) # UserInDB(**user_dict) means: Pass the keys and values of the user_dict directly as key-value arguments.
-    hashed_password = fake_hash_password(login_data.password)
-    if hashed_password != user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password.")
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """ 
+    function to generate a new access token. 
+    Params:
+        data: a dictionary containing the data that you want to encod into the access token.
+        expires_delta: an optional parameter of type timedelta that represents the expiration time of the token. If not provided, the default expiration time is set to 15 minutes.
+    """
+    to_encode = data.copy() # This is done to avoid modifying the original data.
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
 
-    # return user
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print(encoded_jwt)
+    return encoded_jwt
+
+
+
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+
+    if not user:
+        return False
+
+    if not verify_password(password, user.hashed_password):
+        return False
+
+    return user
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI, Depends
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from authentication import authenticate_user
+from jose import JWTError, jwt
+from fake_database import fake_users_db
+from authentication import authenticate_user, get_user, create_access_token, Token, TokenData
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 
@@ -10,6 +21,30 @@ app.title = "API Security"
 
 # This class receives a token as parameter, so what we are saying here is that we have an endpoint called "token" which will return the required information.
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    """ Decode the received token, verify it, and return the current user. If the token is invalid, return an HTTP error right away. """
+    credentials_exception = HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username == None:
+            raise credentials_exception
+        
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    user = get_user(fake_users_db, token_data.username)
+
+    if user == None:
+        raise credentials_exception
+
+    return user
+
 
 
 @app.get("/", tags=["Root"])
@@ -26,18 +61,24 @@ def welcome():
 
 
 @app.get("/users/me", tags=["Users"])
-def user(token: str = Depends(oauth2_scheme)): # Depends is a function which will be executed when we use this endpoint.
-    print(token)
-    return "Hey User!"
+def user(current_user: str = Depends(get_current_user)): # Depends is a function which will be executed when we use this endpoint.
+
+    return current_user
 
 
 
 @app.post("/token", tags=["Token"])
 def login(login_data: OAuth2PasswordRequestForm = Depends()):
     
-    authenticate_user(login_data)
+    user = authenticate_user(fake_users_db, login_data.username, login_data.password)
 
-    return {"access_token": "Tostadas", "token_type": "bearer"}
+    if not user:
+        raise HTTPException(status_code=401, detail="Wrong username or password", headers={"WWW-Authenticate": "Bearer"})
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    return Token(access_token=access_token, token_type="bearer")
 
 
 
